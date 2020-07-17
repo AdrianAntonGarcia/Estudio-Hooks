c96da3daf857608e95b0717cd031141ff5ed1d50
adriton <adrtler@gmail.com>
2020-07-17 17:24:26 +0200

    prueba

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 export const useCounter = (initialState = 10) => {
 	const [state, setState] = useState(initialState);
+	
 
 	const increment = () => {
 		setState(state + 1);
